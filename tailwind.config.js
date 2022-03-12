d636c5fb91114ead56b54737e262bef8faa874cf
Lucas Amazonas <58488230+lucasamazonas@users.noreply.github.com>
2022-03-12 12:16:49 -0400

    Commit de projeto feito

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'Graphik', 'sans-serif'],
+        serif: ['Roboto', 'Merriweather', 'serif']
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
